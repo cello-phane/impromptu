9fb829ef43d5788dea10db990f5084d903159c74
cello-phane <cellophane11578@gmail.com>
2026-06-07 20:53:36 -0700

    Updated test and header.

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 gcc -Wall -O2 -o impromptu.exe impromptu.c -I"C:/Program Files (x86)/SDL2/include/SDL2" -L"C:/Program Files (x86)/SDL2/lib" -lsdl2 -lmingw32 -lSDL2main -lSDL2 -mwindows
+
+#build test_radicaltrig.c
+#gcc -Wall -std=c99 -O2 test_radicaltrig.c radicaltrig.c -lm -o test_radicaltrig -I"C:/Program Files (x86)/SDL2/include/SDL2" -L"C:/Program Files (x86)/SDL2/lib" -lSDL2 -lm
